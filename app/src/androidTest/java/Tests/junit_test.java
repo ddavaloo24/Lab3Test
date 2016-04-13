721d251fb4f7ab576ff4298e4b7e07f6888aa7a7
k2shade <daria.davloo@gmail.com>
2016-04-13 10:43:33 -0700

    First Commit Ha

---
+++ app/src/androidTest/java/Tests/junit_test.java
@@ -0,0 +1,27 @@
+package Tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import blahblah.testie.MainActivity;
+import blahblah.testie.R;
+
+/**
+ * Created by GravityGoose on 4/13/2016.
+ */
+public class junit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity mainActivity;
+
+    public junit_test(){
+        super(MainActivity.class);
+    }
+
+    public void test_first(){
+        mainActivity = getActivity();
+        TextView textView = (TextView)mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Hello World!",tester);
+    }
+}
